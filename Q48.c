3ef196c299fdd74bbd38eac45da02a0e91a4f1e4
Kunal-Error404 <kunalsaini73112@gmail.com>
2026-02-24 20:41:25 +0530

    completed

---
+++ Q48.c
@@ -0,0 +1,43 @@
+/*Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 104].
+1 <= Node.val <= 50
+0 <= val <= 50*/
+struct ListNode* removeElements(struct ListNode* head, int val) {
+    struct ListNode *p, *q;
+    while (head != NULL && head->val == val) {
+        p = head;
+        head = head->next;
+        free(p);
+    }
+    q = head;
+    while (q != NULL && q->next != NULL) {
+        if (q->next->val == val) {
+            p = q->next;
+            q->next = p->next;
+            free(p);
+        } else {
+            q = q->next;
+        }
+    }
+    return head;
+}
