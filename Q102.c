0d91c1f1aae5d62303cb0e3650fa793a14f7740c
Kunal-Error404 <kunalsaini73112@gmail.com>
2026-03-29 00:01:55 +0530

    .

--- Q102.c
+++ Q102.c
@@ -18,3 +18,50 @@ Output:
 
 Explanation:
 At each level, the rightmost node is visible from the right view.
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    
+    int arr[1000];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    
+    int q[1000];
+    int idx[1000];
+    int front = 0, back = 0;
+    
+    if (arr[0] != -1) {
+        q[back] = 0;
+        idx[back++] = 0;
+    }
+    
+    int first = 1;
+    while (front < back) {
+        int size = back - front;
+        int last = -1;
+        
+        for (int i = 0; i < size; i++) {
+            int node = q[front];
+            int ni = idx[front++];
+            
+            if (arr[ni] != -1)
+                last = arr[ni];
+            
+            int l = 2 * node + 1;
+            int r = 2 * node + 2;
+            
+            if (l < n && arr[l] != -1) { q[back] = l; idx[back++] = l; }
+            if (r < n && arr[r] != -1) { q[back] = r; idx[back++] = r; }
+        }
+        
+        if (last != -1) {
+            if (!first) printf(" ");
+            printf("%d", last);
+            first = 0;
+        }
+    }
+    printf("\n");
+    return 0;
+}
