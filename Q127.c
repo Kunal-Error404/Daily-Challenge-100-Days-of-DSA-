1eb15b61a8adefe240296094199b1ab3a2775685
Kunal-Error404 <kunalsaini73112@gmail.com>
2026-04-23 23:44:19 +0530

    .

---
+++ Q127.c
@@ -0,0 +1,35 @@
+/*Problem: Implement Bubble Sort - Implement the algorithm.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the sorted array or search result
+
+Example:
+Input:
+5
+64 34 25 12 22
+
+Output:
+12 22 25 34 64*/
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1]) {
+                int t = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = t;
+            }
+    for (int i = 0; i < n; i++)
+        printf("%d%s", arr[i], i < n - 1 ? " " : "\n");
+    return 0;
+}
